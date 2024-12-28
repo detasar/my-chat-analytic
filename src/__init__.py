e8925be3a454c3006c63ae9f3295699d58aac2ea
Davut Emre <detasar@gmail.com>
2024-12-29 02:00:59 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+src package initialization.
+
+This package contains:
+- db.py (database and model definitions)
+- logger.py (asynchronous logging logic)
+- sentiment_intent_analyzer.py (zero-shot classification pipeline)
+- main.py (entry point to run the entire flow)
+"""
